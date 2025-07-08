f5e40127a78eea39ac3ad9c73072b31ac0b30127
itsmert <104797619+itsmert@users.noreply.github.com>
2025-07-08 16:45:20 +0300

    Frontend Part Will Be Starting to implement, there will be some figuresmissing on backend it will be fine.

--- JForce/JForce-Backend/src/main/java/JForce/JForce/Controller/InventoryController.java
+++ JForce/JForce-Backend/src/main/java/JForce/JForce/Controller/InventoryController.java
@@ -6,11 +6,8 @@ import JForce.JForce.Service.InventoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/inventory")
